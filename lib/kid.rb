0e09a1448538c5cee100e31d0794510b2380d1c5
Whittney Davis <wcdavis89@yahoo.com>
2019-12-04 00:10:06 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,8 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
-  include Dance 
-  extend MetaDancing
+  extend
   attr_accessor :name
   
   def initialize(name)
