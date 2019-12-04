62507983e3c78f44c31144fa257e5c7e9f894dff
Whittney Davis <wcdavis89@yahoo.com>
2019-12-04 00:11:32 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,7 +4,7 @@ require_relative './dance_module.rb'
 class Dancer
   extend FancyDance::ClassMethods 
   include FancyDance::InstanceMethods
-end
+
   attr_accessor :name 
   def initialize(name)
     @name=name
