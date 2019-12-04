b3a39e9ffa823ecd58de0bf73c3c28641a13871d
Whittney Davis <wcdavis89@yahoo.com>
2019-12-04 00:02:45 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -14,3 +14,9 @@ module FancyDance
        "Thank you, thank you. It was a pleasure to dance for you all."
      end
    end
+   module ClassMethods
+     def metadata
+       "This class produces objects that love to dance."
+     end
+   end
+ end
