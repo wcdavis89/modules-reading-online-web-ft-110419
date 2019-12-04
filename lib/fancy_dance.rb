abf4ffab6d66e5dc8a3dc2b5fd864765f115cd5e
Whittney Davis <wcdavis89@yahoo.com>
2019-12-04 00:24:03 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -8,7 +8,7 @@ module FancyDance
        "Look how high I'm jumping!"
      end
      def pirouette
-       "I am doing a pirouette"
+       "I'm doing a pirouette"
      end
      def take_a_bow
        "Thank you, thank you. It was a pleasure to dance for you all."
